90c34b33a753306a69948696c112bea976a74151
Ganesh Reddy <49718040+ganesh-gonnuru@users.noreply.github.com>
2020-05-07 20:52:55 +0530

    Admission Enquiry & Admission Application RestControllers

--- src/main/java/com/synectiks/admission/ems/rest/AdmissionApplicationRestController.java
+++ src/main/java/com/synectiks/admission/ems/rest/AdmissionApplicationRestController.java
@@ -1,19 +1,29 @@
 package com.synectiks.admission.ems.rest;
 
-import java.util.List;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.synectiks.admission.business.service.CmsAdmissionApplicationService;
 import com.synectiks.admission.domain.AdmissionApplication;
 import com.synectiks.admission.domain.vo.CmsAdmissionApplicationVo;
+import com.synectiks.admission.repository.AdmissionApplicationRepository;
+import com.synectiks.admission.service.util.CommonUtil;
+import com.synectiks.admission.web.rest.errors.BadRequestAlertException;
+import com.synectiks.admission.web.rest.util.HeaderUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import static graphql.servlet.GraphQLServlet.log;
 
 @RestController
 @RequestMapping("/api")
@@ -21,24 +31,117 @@ public class AdmissionApplicationRestController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private static final String ENTITY_NAME = "admissionApplication";
+
 	@Autowired
 	private CmsAdmissionApplicationService cmsAdmissionApplicationService;
-	
-	@RequestMapping(method = RequestMethod.GET, value = "/cmsadmission-application-by-filters")
-    public List<CmsAdmissionApplicationVo> getCmsAdmissionApplicationListOnFilterCriteria(@RequestParam Map<String, String> dataMap) throws Exception {
-        logger.debug("Rest request to get list of CmsAdmissionApplications based on filter criteria");
-        Long branchId = Long.parseLong(dataMap.get("branchId"));
-        Long academicYearId = Long.parseLong(dataMap.get("academicYearId"));
-        List<CmsAdmissionApplicationVo> list = this.cmsAdmissionApplicationService.getCmsAdmissionApplicationList(branchId, academicYearId);
-        return list;
+
+    @Autowired
+    private AdmissionApplicationRepository admissionApplicationRepository;
+
+    @PostMapping("/cmsadmissionapplication-bulk-load")
+    public List<CmsAdmissionApplicationVo> bulkLoad(@RequestBody List<CmsAdmissionApplicationVo> list) throws Exception {
+        List<CmsAdmissionApplicationVo> failedRecords = new ArrayList<>();
+        for(CmsAdmissionApplicationVo cmsAdmissionApplicationVo: list) {
+            try {
+                createAdmissionApplication(cmsAdmissionApplicationVo);
+            }catch(Exception e) {
+                failedRecords.add(cmsAdmissionApplicationVo);
+                log.error("Exception. Saving of admissionApplication failed. ", e);
+            }
+        }
+
+        return failedRecords;
     }
-    
-    @RequestMapping(method = RequestMethod.GET, value = "/admission-application-by-filters")
-    public List<AdmissionApplication> getAdmissionApplicationListOnFilterCriteria(@RequestParam Map<String, String> dataMap) throws Exception {
-        logger.debug("Rest request to get list of AdmissionApplications based on filter criteria");
+
+    @RequestMapping(method = RequestMethod.GET, value = "/cmsadmission-application-by-filters")
+    public List<AdmissionApplication> getCmsAdmissionApplicationListOnFilterCriteria(@RequestParam Map<String, String> dataMap) throws Exception {
+        logger.debug("Rest request to get list of CmsAdmissionApplication based on filter criteria");
         Long branchId = Long.parseLong(dataMap.get("branchId"));
         Long academicYearId = Long.parseLong(dataMap.get("academicYearId"));
         List<AdmissionApplication> list = this.cmsAdmissionApplicationService.getAdmissionApplicationList(branchId, academicYearId);
         return list;
+    }
+
+
+
+    @PostMapping("/cmsadmission-application")
+    public ResponseEntity<CmsAdmissionApplicationVo> createAdmissionApplication(@Valid @RequestBody CmsAdmissionApplicationVo cmsAdmissionApplicationVo) throws Exception {
+        log.debug("REST request to save a admissionApplication : {}", cmsAdmissionApplicationVo);
+        if (cmsAdmissionApplicationVo.getId() != null) {
+            throw new BadRequestAlertException("A new admissionApplication cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        AdmissionApplication admissionApplication = CommonUtil.createCopyProperties(cmsAdmissionApplicationVo, AdmissionApplication.class);
+        AdmissionApplication result = admissionApplicationRepository.save(admissionApplication);
+        CmsAdmissionApplicationVo vo = CommonUtil.createCopyProperties(admissionApplication, CmsAdmissionApplicationVo.class);
+        return ResponseEntity.created(new URI("/api/cmsadmission-application/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
+            .body(vo);
+    }
+
+    @PutMapping("/cmsadmission-application")
+    public ResponseEntity<CmsAdmissionApplicationVo> updateAdmissionApplication(@Valid @RequestBody CmsAdmissionApplicationVo cmsAdmissionApplicationVo) throws URISyntaxException {
+        log.debug("REST request to update a admissionApplication : {}", cmsAdmissionApplicationVo);
+        if (cmsAdmissionApplicationVo.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        AdmissionApplication admissionApplication = CommonUtil.createCopyProperties(cmsAdmissionApplicationVo, AdmissionApplication.class);
+        AdmissionApplication result = admissionApplicationRepository.save(admissionApplication);
+        CmsAdmissionApplicationVo vo = CommonUtil.createCopyProperties(admissionApplication, CmsAdmissionApplicationVo.class);
+
+        return ResponseEntity.created(new URI("/api/cmsadmission-application/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
+            .body(vo);
+    }
+
+
+    @GetMapping("/cmsadmission-application")
+    public List<CmsAdmissionApplicationVo> getAllAdmissionApplication(@RequestParam Map<String, String> dataMap) {
+        List<AdmissionApplication> list = null;
+        List<CmsAdmissionApplicationVo> ls = null;
+
+        AdmissionApplication obj = new AdmissionApplication();
+        boolean isFilter = false;
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("id"))) {
+            obj.setId(Long.parseLong(dataMap.get("id")));
+            isFilter = true;
+        }
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("branchId"))) {
+//    		Branch branch = this.commonService.getBranchById(Long.parseLong(dataMap.get("branchId")));
+            obj.setBranchId(Long.parseLong(dataMap.get("branchId")));
+            isFilter = true;
+        }
+
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("academicYearId"))) {
+            obj.setAcademicYearId(Long.parseLong(dataMap.get("academicYearId")));
+            isFilter = true;
+        }
+
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("sourceOfApplication"))) {
+            isFilter = true;
+            String name = dataMap.get("sourceOfApplication");
+            StringTokenizer token = new StringTokenizer(name, " ");
+            int cnt = 0;
+            while(token.hasMoreTokens()) {
+                if(cnt == 0) {
+                    obj.setSourceOfApplication(token.nextToken());
+                }
+                cnt++;
+            }
+//        	ls = getStudentListByName(name) ;
+        }
+//        List<Teacher> list = null;
+        if(isFilter) {
+            list = this.admissionApplicationRepository.findAll(Example.of(obj), Sort.by(Sort.Direction.DESC, "id"));
+        }else {
+            list = this.admissionApplicationRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        }
+        ls = new ArrayList<>();
+        for(AdmissionApplication fc: list) {
+            CmsAdmissionApplicationVo vo = CommonUtil.createCopyProperties(fc, CmsAdmissionApplicationVo.class);
+            ls.add(vo);
+        }
+
+        return ls;
     }
 }
--- src/main/java/com/synectiks/admission/ems/rest/AdmissionEnquiryRestController.java
+++ src/main/java/com/synectiks/admission/ems/rest/AdmissionEnquiryRestController.java
@@ -1,18 +1,30 @@
 package com.synectiks.admission.ems.rest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
+import com.synectiks.admission.domain.AdmissionEnquiry;
+import com.synectiks.admission.repository.AdmissionEnquiryRepository;
+import com.synectiks.admission.service.util.CommonUtil;
+import com.synectiks.admission.web.rest.errors.BadRequestAlertException;
+import com.synectiks.admission.web.rest.util.HeaderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.synectiks.admission.business.service.CmsAdmissionEnquiryService;
 import com.synectiks.admission.domain.vo.CmsAdmissionEnquiryVo;
+
+import javax.validation.Valid;
+import static graphql.servlet.GraphQLServlet.log;
 
 @RestController
 @RequestMapping("/api")
@@ -20,10 +32,29 @@ public class AdmissionEnquiryRestController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private static final String ENTITY_NAME = "admissionEnquiry";
 	@Autowired
-	CmsAdmissionEnquiryService cmsAdmissionEnquiryService;
-	
-	@RequestMapping(method = RequestMethod.GET, value = "/cmsadmission-enquiry-by-filters")
+    private CmsAdmissionEnquiryService cmsAdmissionEnquiryService;
+
+    @Autowired
+    private AdmissionEnquiryRepository admissionEnquiryRepository;
+
+    @PostMapping("/cmsadmissionenquiry-bulk-load")
+    public List<CmsAdmissionEnquiryVo> bulkLoad(@RequestBody List<CmsAdmissionEnquiryVo> list) throws Exception {
+        List<CmsAdmissionEnquiryVo> failedRecords = new ArrayList<>();
+        for(CmsAdmissionEnquiryVo cmsAdmissionEnquiryVo: list) {
+            try {
+                createAdmissionEnquiry(cmsAdmissionEnquiryVo);
+            }catch(Exception e) {
+                failedRecords.add(cmsAdmissionEnquiryVo);
+                log.error("Exception. Saving of admissionEnquiry failed. ", e);
+            }
+        }
+
+        return failedRecords;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/cmsadmission-enquiry-by-filters")
     public List<CmsAdmissionEnquiryVo> getCmsAdmissionEnquiryListOnFilterCriteria(@RequestParam Map<String, String> dataMap) throws Exception {
         logger.debug("Rest request to get list of CmsAdmissionEnquiry based on filter criteria");
         Long branchId = Long.parseLong(dataMap.get("branchId"));
@@ -32,5 +63,108 @@ public class AdmissionEnquiryRestController {
         List<CmsAdmissionEnquiryVo> list = this.cmsAdmissionEnquiryService.getAdmissionEnquiryList(branchId, academicYearId, enquiryStatus);
         return list;
     }
-	
+
+
+
+    @PostMapping("/cmsadmission-enquiry")
+    public ResponseEntity<CmsAdmissionEnquiryVo> createAdmissionEnquiry(@Valid @RequestBody CmsAdmissionEnquiryVo cmsAdmissionEnquiryVo) throws Exception {
+        log.debug("REST request to save a admissionEnquiry : {}", cmsAdmissionEnquiryVo);
+        if (cmsAdmissionEnquiryVo.getId() != null) {
+            throw new BadRequestAlertException("A new admissionEnquiry cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        AdmissionEnquiry admissionEnquiry = CommonUtil.createCopyProperties(cmsAdmissionEnquiryVo, AdmissionEnquiry.class);
+        AdmissionEnquiry result = admissionEnquiryRepository.save(admissionEnquiry);
+        CmsAdmissionEnquiryVo vo = CommonUtil.createCopyProperties(admissionEnquiry, CmsAdmissionEnquiryVo.class);
+        return ResponseEntity.created(new URI("/api/cmsadmission-enquiry/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
+            .body(vo);
+    }
+
+    @PutMapping("/cmsadmission-enquiry")
+    public ResponseEntity<CmsAdmissionEnquiryVo> updateAdmissionEnquiry(@Valid @RequestBody CmsAdmissionEnquiryVo cmsAdmissionEnquiryVo) throws URISyntaxException {
+        log.debug("REST request to update a admissionEnquiry : {}", cmsAdmissionEnquiryVo);
+        if (cmsAdmissionEnquiryVo.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        AdmissionEnquiry admissionEnquiry = CommonUtil.createCopyProperties(cmsAdmissionEnquiryVo, AdmissionEnquiry.class);
+        AdmissionEnquiry result = admissionEnquiryRepository.save(admissionEnquiry);
+        CmsAdmissionEnquiryVo vo = CommonUtil.createCopyProperties(admissionEnquiry, CmsAdmissionEnquiryVo.class);
+
+        return ResponseEntity.created(new URI("/api/cmsadmission-enquiry/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
+            .body(vo);
+    }
+
+
+    @GetMapping("/cmsadmission-enquiry")
+    public List<CmsAdmissionEnquiryVo> getAllAdmissionEnquiry(@RequestParam Map<String, String> dataMap) {
+        List<AdmissionEnquiry> list = null;
+        List<CmsAdmissionEnquiryVo> ls = null;
+
+        AdmissionEnquiry obj = new AdmissionEnquiry();
+        boolean isFilter = false;
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("id"))) {
+            obj.setId(Long.parseLong(dataMap.get("id")));
+            isFilter = true;
+        }
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("branchId"))) {
+//    		Branch branch = this.commonService.getBranchById(Long.parseLong(dataMap.get("branchId")));
+            obj.setBranchId(Long.parseLong(dataMap.get("branchId")));
+            isFilter = true;
+        }
+
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("departmentId"))) {
+            obj.setDepartmentId(Long.parseLong(dataMap.get("departmentId")));
+            isFilter = true;
+        }
+
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("batchId"))) {
+            obj.setBatchId(Long.parseLong(dataMap.get("batchId")));
+            isFilter = true;
+        }
+
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("stateId"))) {
+            obj.setStateId(Long.parseLong(dataMap.get("stateId")));
+            isFilter = true;
+        }
+
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("cityId"))) {
+            obj.setCityId(Long.parseLong(dataMap.get("cityId")));
+            isFilter = true;
+        }
+
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("academicYearId"))) {
+            obj.setAcademicYearId(Long.parseLong(dataMap.get("academicYearId")));
+            isFilter = true;
+        }
+
+        if(!CommonUtil.isNullOrEmpty(dataMap.get("studentName"))) {
+            isFilter = true;
+            String name = dataMap.get("studentName");
+            StringTokenizer token = new StringTokenizer(name, " ");
+            int cnt = 0;
+            while(token.hasMoreTokens()) {
+                if(cnt == 0) {
+                    obj.setStudentName(token.nextToken());
+                }
+                cnt++;
+            }
+//        	ls = getStudentListByName(name) ;
+        }
+//        List<Teacher> list = null;
+        if(isFilter) {
+            list = this.admissionEnquiryRepository.findAll(Example.of(obj), Sort.by(Sort.Direction.DESC, "id"));
+        }else {
+            list = this.admissionEnquiryRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        }
+        ls = new ArrayList<>();
+        for(AdmissionEnquiry fc: list) {
+            CmsAdmissionEnquiryVo vo = CommonUtil.createCopyProperties(fc, CmsAdmissionEnquiryVo.class);
+            ls.add(vo);
+        }
+
+        return ls;
+    }
+
+
 }
